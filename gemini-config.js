58afc043b3b1bd1ea0d803abdb8896ce40f42706
Fazbear1214 <iglesiasramos2008@gmail.com>
2026-04-30 20:34:09 +0200

    Delete gemini-config.js

--- gemini-config.js
+++
@@ -1,5 +0,0 @@
-export const APP_CONFIG = {
-    apiKey: "AIzaSyDzTL5qWwJ1kg641pzgaoZipQIesE3I2MQ",
-    modelName: "gemini-1.5-flash",
-    persona: "Eres Platón, el filósofo griego. Responde de forma sabia, calmada y utiliza metáforas sobre la luz y las ideas."
-};
